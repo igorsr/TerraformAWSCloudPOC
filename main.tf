ea2236ceedbc97e5968ca5978d25b32e0c042902
igorsr <igor.souza.russo@gmail.com>
2022-12-10 08:11:40 -0300

    create vpc

--- main.tf
+++ main.tf
@@ -16,3 +16,42 @@ provider "aws" {
   version = "~> 2.36.0"
 }
 
+/*
+module "bootstrap" {
+  source                      = "./modules/bootstrap"
+  name_of_s3_bucket           = "your_globally_unique_bucket_name"
+  dynamo_db_table_name        = "aws-locks"
+  iam_user_name               = "IamUser"
+  ado_iam_role_name           = "IamRole"
+  aws_iam_policy_permits_name = "IamPolicyPermits"
+  aws_iam_policy_assume_name  = "IamPolicyAssume"
+}
+*/
+
+# Build the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  tags = {
+    Name      = "Vpc"
+    Terraform = "true"
+  }
+}
+# Build route table 1
+resource "aws_route_table" "route_table1" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable1"
+    Terraform = "true"
+  }
+}
+# Build route table 2
+resource "aws_route_table" "route_table2" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable2"
+    Terraform = "true"
+  }
+}
+
+
