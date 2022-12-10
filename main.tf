77e0f4be583a7d95d3944543c2fb60ce60fddf26
igorsr <igor.souza.russo@gmail.com>
2022-12-10 08:01:04 -0300

    first

---
+++ main.tf
@@ -0,0 +1,18 @@
+# Require TF version to be same as or greater than 0.12.13
+terraform {
+  required_version = ">=0.12.13"
+  backend "s3" {
+    bucket         = "igorrusso-terraform-test"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
+}
+
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.36.0"
+}
+
