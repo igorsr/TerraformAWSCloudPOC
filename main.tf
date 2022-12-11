f8a3c87e347294771cffcaff7ffc2665daeb3bf2
igorsr <igor.souza.russo@gmail.com>
2022-12-11 17:31:19 -0300

    creating vpc with public subnet

--- main.tf
+++ main.tf
@@ -27,32 +27,59 @@ module "bootstrap" {
 
 
 
-/*
-# Build the VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.1.0.0/16"
-  instance_tenancy     = "default"
+# Criação da VPC
+resource "aws_vpc" "igorrusso_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
   tags = {
-    Name      = "Vpc"
-    Terraform = "true"
-  }
-}
-# Build route table 1
-resource "aws_route_table" "route_table1" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "RouteTable1"
-    Terraform = "true"
-  }
-}
-# Build route table 2
-resource "aws_route_table" "route_table2" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "RouteTable2"
-    Terraform = "true"
+    Name = "igorrusso_vpc"
   }
 }
 
+# Criação da Subnet Pública
+resource "aws_subnet" "igorrusso_public_subnet" {
+  vpc_id     = aws_vpc.igorrusso_vpc.id
+  cidr_block = "10.0.1.0/24"
 
-*/
+  tags = {
+    Name = "igorrusso_public_subnet"
+  }
+}
+
+# Criação do Internet Gateway
+resource "aws_internet_gateway" "igorrusso_igw" {
+  vpc_id = aws_vpc.igorrusso_vpc.id
+
+  tags = {
+    Name = "igorrusso_igw"
+  }
+}
+
+# Criação da Tabela de Roteamento
+resource "aws_route_table" "igorrusso_rt" {
+  vpc_id = aws_vpc.igorrusso_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igorrusso_igw.id
+  }
+
+  tags = {
+    Name = "igorrusso_rt"
+  }
+}
+
+# Criação da Rota Default para Acesso à Internet
+resource "aws_route" "igorrusso_routetointernet" {
+  route_table_id            = aws_route_table.igorrusso_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.igorrusso_igw.id
+}
+
+
+# Associação da Subnet Pública com a Tabela de Roteamento
+resource "aws_route_table_association" "igorrusso_pub_association" {
+  subnet_id      = aws_subnet.igorrusso_public_subnet.id
+  route_table_id = aws_route_table.igorrusso_rt.id
+}
